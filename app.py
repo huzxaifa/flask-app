8b5ddfa026cd1b9ab4cee5c0596f4bd0d5359ded
huzxaifa <i210527@nu.edu.pk>
2025-09-04 12:23:20 +0500

    Intentionally breaking the build 2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flaskx import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Required for sessions & flash messages
